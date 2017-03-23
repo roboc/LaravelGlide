cbff6fd2ab691a71630dfdb04728b166d9c8e37a
Roberts <roberts@cubesystems.lv>
2017-03-23 19:57:05 +0200

    Fix typo in environment variable name

--- config/laravel-glide.php
+++ config/laravel-glide.php
@@ -2,28 +2,28 @@
 
 return [
 
-    /**
+    /*
      * By default Glide uses the GD library. However you can also use Glide with ImageMagick
      * if the Imagick PHP extension is installed.
      */
     'driver' => env( 'GLIDE_DRIVER', 'gd' ),
 
-    /**
+    /*
      * Name of the disk where source images are stored
      */
     'source_disk' => env( 'GLIDE_SOURCE_DISK', 'local' ),
 
-    /**
+    /*
      * Name of the disk where processed images must be stored
      */
-    'cache_disk' => env( 'GLIDE_SOURCE_DISK', 'public' ),
+    'cache_disk' => env( 'GLIDE_CACHE_DISK', 'public' ),
 
-    /**
+    /*
      * Name of the disk where watermarks are stored
      */
     'watermarks_disk' => env( 'GLIDE_WATERMARKS_DISK', 'local' ),
 
-    /**
+    /*
      * In certain situations you may want to define default image manipulations.
      * For example, maybe you want to specify that all images are outputted as JPEGs (fm=jpg).
      * Or maybe you have a watermark that you want added to all images.
@@ -31,7 +31,7 @@ return [
      */
     'defaults' => [],
 
-    /**
+    /*
      * Glide also makes it possible to define groups of defaults, known as presets.
      * This is helpful if you have standard image manipulations that you use throughout your app.
      */
